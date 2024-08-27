f69667f9b86e242858bdb52c82d93654819859fb
Silsantiago <est.manuels.silva@unimilitar.edu.co>
2024-08-27 14:51:24 -0500

    Create pg2.java

---
+++ P1/pg2.java
@@ -0,0 +1,14 @@
+//con la misma clase de la pregunta anterior crear una sobrecarga del constructor y crear un objeto con cada una de sus formas del constructor
+public class fracciones{
+    private numerador;
+    private denominador;
+    
+public fraccion (int numerador, int denominador){
+    this.numerador = numerador;
+    if (denominador == 0);
+   public fraccion (int numerador){
+       this (numerador, 1);
+       
+   }
+}
+}
