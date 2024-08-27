8fb20a438179a4504858aa92c2d1d484608d4658
Silsantiago <est.manuels.silva@unimilitar.edu.co>
2024-08-27 15:03:00 -0500

    Create pg3.java

---
+++ P1/pg3.java
@@ -0,0 +1,40 @@
+//con la misma clase crear un metodo puro que me permita sumar dos fracciones y que me devuelva su resultado
+public class fracciones{
+    private numerador;
+    private denominador;
+    
+public fraccion (int numerador, int denominador){
+    this.numerador = numerador;
+    if (denominador == 0);
+   public fraccion (int numerador){
+       this (numerador, 1);
+       
+    public fraccion (){
+            this(0,1);
+    }                
+      public int  getNumerador(){
+          return numerador;
+      }
+      public void setNumerador(int numerador){
+          this.numerador = numeradir;
+     
+      }
+      public int getDenominador(){
+          if(denominador == 0)
+              
+              
+      public string tostring(){
+          return numerador + "/" + denominador;
+          
+      }
+      public static fraccion sumar (fraccion f1, fraccion f2){
+          
+          
+      system.out.println("resultado de la suma");
+          
+      }
+      
+      }
+   }
+}
+}
