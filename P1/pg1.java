7f5be6c0e4187e907737ee15e2daa59d0c9719a5
Silsantiago <est.manuels.silva@unimilitar.edu.co>
2024-08-27 14:35:36 -0500

    Create pg1.java

---
+++ P1/pg1.java
@@ -0,0 +1,15 @@
+
+//Crear una clase llamada fraccion atributios correspondientes a numerador y denominador, aplicar encapsulamniento y los metodo get y sed para manejar objetos de esa clase
+public class fraccion{
+    private int numerador;
+    private int denominador;
+ 
+    public int getNumerador; 
+    return numerador;
+    public void setNumerador(int numerador); 
+     this.numerador=numerador
+    
+       
+       public int getDenominador;
+    public void setDenominador;
+}
